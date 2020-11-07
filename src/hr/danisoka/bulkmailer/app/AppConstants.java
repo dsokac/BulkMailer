a81f0ab774cc2aa7ec3ce7dbcf374d5b43cdd7c3
dsokac <dsokac@foi.hr>
2020-11-07 12:58:29 +0100

    add folder for attempts / reports

--- src/hr/danisoka/bulkmailer/app/AppConstants.java
+++ src/hr/danisoka/bulkmailer/app/AppConstants.java
@@ -14,6 +14,7 @@ public final class AppConstants {
             String TEMPLATES_FOLDER = DATA_FOLDER + File.separator + "templates";
             String CSV_FOLDER = DATA_FOLDER + File.separator + "csv files";
             String DB_FOLDER = DATA_FOLDER + File.separator + "db";
+            String ATTEMPT_FOLDER = DATA_FOLDER + File.separator + "reports";
         }
         
         public interface Files {
