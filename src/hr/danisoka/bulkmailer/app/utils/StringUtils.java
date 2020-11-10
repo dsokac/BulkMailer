64abc9a9e4a5939bd286c9a6911eb57093198edd
dsokac <dsokac@foi.hr>
2020-11-10 23:47:00 +0100

    handle parsing string of items separated by comma

--- src/hr/danisoka/bulkmailer/app/utils/StringUtils.java
+++ src/hr/danisoka/bulkmailer/app/utils/StringUtils.java
@@ -1,10 +1,17 @@
 package hr.danisoka.bulkmailer.app.utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public final class StringUtils {
     public static List<String> getItemsFromString(String delimiter, String value) {
-        return Arrays.asList(value.split(delimiter));
+        String[] array = value.trim().split(delimiter);
+        List<String> list = Arrays.asList(array);
+        List<String> processedList = new ArrayList<>();
+        for(String item : list){
+            processedList.add(item.trim());
+        }
+        return array.length == 1 && array[0].isEmpty() ? null : processedList;
     }
 }
